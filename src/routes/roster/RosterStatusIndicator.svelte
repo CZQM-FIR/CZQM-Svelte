ca9e9bca4eb58e2206b08d0273476595232994a8
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-03-31 21:52:56 -0400

    make roster status less blinding

--- src/routes/roster/RosterStatusIndicator.svelte
+++ src/routes/roster/RosterStatusIndicator.svelte
@@ -5,9 +5,15 @@
 {#if roster === -1}
   <td class="text-center"> --- </td>
 {:else if roster === -2}
-  <td class="text-center bg-error text-error-content">In Training</td>
+  <td class="text-center text-error-content"
+    ><div class="badge badge-error badge-lg">In Training</div></td
+  >
 {:else if roster === 0}
-  <td class="text-center bg-warning text-warning-content">Solo</td>
+  <td class="text-center text-warning-content"
+    ><div class="badge badge-warning badge-lg">Solo</div></td
+  >
 {:else if roster === 1}
-  <td class="text-center bg-success text-success-content">Certified</td>
+  <td class="text-center text-success-content"
+    ><div class="badge badge-success badge-lg">Certified</div></td
+  >
 {/if}
